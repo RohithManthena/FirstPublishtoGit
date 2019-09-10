af0f85a0be82ddac14b980f7b1f537b05d53058b
RohithManthena <smanthena@mail.usf.edu>
2019-09-10 16:13:43 -0400

    Push

---
+++ FirstOne/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey This is the first one");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
